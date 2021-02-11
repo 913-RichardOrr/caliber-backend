6ee73ee83efed7d07ec2fe6ba44b7f82ce3af996
Medbh Campbell <medbh.campbell53@gmail.com>
2021-02-11 15:03:05 -0600

    function and test stubs for associateLambda

---
+++ associateLambda/index.ts
@@ -0,0 +1,32 @@
+// lambda handler function
+
+interface AssocEvent {
+    path: string;
+    body?: string;
+    method?: string;
+}
+
+//figures out what http method has been called: GET, PUT, PATCH
+//call the relevant helper function
+//return the relevant object
+export const handler = async (event: AssocEvent): Promise<any> => {
+
+}
+
+//method is get
+//get the note and technical status for that person for that week
+export const getAssociate = async (): Promise<any> => {
+
+}
+
+//method is put
+//create the note and technical status for that person for that week
+export const putAssociate = async (): Promise<any> => {
+
+}
+
+//method is patch
+//update an existing note or status
+export const patchAssociate = async (): Promise<any> => {
+
+}
---
+++ associateLambda/test/index.test.ts
@@ -0,0 +1,19 @@
+//Tests for associate lambda handler and helpers
+
+import { handler, getAssociate, putAssociate, patchAssociate } from '../index';
+
+describe('tests for handler', () => {
+
+});
+
+describe('tests for getAssociate', () => {
+
+});
+
+describe('tests for putAssociate', () => {
+
+});
+
+describe('tests for patchAssociate', () => {
+
+});
