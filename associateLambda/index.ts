ff8eb633fa27a794940e3c33ab690d8b40e3022a
kathrync23 <kathrynclark16@augustana.edu>
2021-02-11 16:27:51 -0600

    added more stuff to test

--- associateLambda/index.ts
+++ associateLambda/index.ts
@@ -1,32 +1,23 @@
 // lambda handler function
+import * as associateLambda from './associateService';
 
-interface AssocEvent {
+export interface AssocEvent {
   path: string;
-  body?: string;
+  body?: any;
   method?: string;
 }
 
 //figures out what http method has been called: GET, PUT, PATCH
 //call the relevant helper function
 //return the relevant object
-export const handler = async (event: AssocEvent): Promise<any> => {};
-
-//method is get
-//get the note and technical status for that person for that week
-export const getAssociate = async (): Promise<any> => {};
-
-//method is put
-//create the note and technical status for that person for that week
-export const putAssociate = async (): Promise<any> => {};
-
-//method is patch
-//update an existing note or status
-export const patchAssociate = async (): Promise<any> => {};
+export async function handler(event: AssocEvent): Promise<any> {
+  return {};
+}
 
 export class qcFeedback {
-  batchId: string;
-  weekId: number;
-  associateId: string;
-  qcNote: string;
-  qcTechnicalStatus: number;
+  batchId: string = '';
+  weekId: number = -1;
+  associateId: string = '';
+  qcNote: string = '';
+  qcTechnicalStatus: number = -1;
 }
