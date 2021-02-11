99d9ee3604ba0f6707557a1c59ee35323f28b108
kathrync23 <kathrynclark16@augustana.edu>
2021-02-11 15:39:51 -0600

    started the tests in liveshare, will work separately for the rest

--- associateLambda/test/index.test.ts
+++ associateLambda/test/index.test.ts
@@ -1,19 +1,59 @@
 //Tests for associate lambda handler and helpers
 
 import { handler, getAssociate, putAssociate, patchAssociate } from '../index';
+let testEvent = {
+    path: 'path',
+    body?: {"1":1},
+    method?: 'method'
+}
 
 describe('tests for handler', () => {
-
+    
+  beforeEach( () => {
+    {
+        testEvent.path = '/something';
+        testEvent.body = {"1":1};
+        testEvent.method = 'PUT';
+    }
+  })
+      
+  test('test handler can differentiate between get/put/patch', async () => {
+    thishandler = jest.fn()
+  });
 });
 
-describe('tests for getAssociate', () => {
+describe('tests for getAssociate', async () => {
+  testEvent.path = '/something';
+  
 
+  test('that getAssociate gets associate')
 });
 
 describe('tests for putAssociate', () => {
+  testEvent.path = 'idk';
+  testEvent.body = { note: 'test note', status: 2 };
+  test('that putAssociate does things....', () => {
 
+  });
+    {
+        testEvent.path = '1';
+    }
+  })
+      
+  test('test handler can differentiate between get/put/patch', async () => {
+    
+  });
 });
 
-describe('tests for patchAssociate', () => {
+describe('tests for getAssociate', async () => {});
 
+describe('tests for putAssociate', () => {
+  let testEvent = {
+    path:
+  }
+  test('that putAssociate does things....', () => {
+
+  });
 });
+
+describe('tests for patchAssociate', () => {});
