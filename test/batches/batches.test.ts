426fd6895c2c196ead8adb3eccafb66af4829ffb
Jonathan Dawson <dawsonjed@gmail.com>
2021-02-11 16:05:18 -0600

    initial commit for test

---
+++ test/batches/batches.test.ts
@@ -0,0 +1,16 @@
+//welcome we are testing the batches endpoint.
+
+//imports
+
+
+describe('Batches Test Suite', () => {
+  test('GET Batches', () => {
+    //test goes here
+  });
+});
+
+// the next lines will go into the actual function, not the test
+/**
+ * getBatches takes in a trainer ID (there is no trainer ID, only trainer email) and returns a list of batch objects from the caliber mock api
+ * @param event
+ */
