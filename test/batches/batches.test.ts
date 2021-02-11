183c234d039fa28b5616a783200493fe430c95b5
Jonathan Dawson <dawsonjed@gmail.com>
2021-02-11 17:07:00 -0600

    make the test results

--- test/batches/batches.test.ts
+++ test/batches/batches.test.ts
@@ -1,43 +1,108 @@
 //welcome we are testing the batches endpoint.
 
 //imports
-import axios from 'axios';
+import axios from "axios";
+// according to the docs, you have to mock axios:  https://jestjs.io/docs/en/mock-functions
+jest.mock("axios");
 
-
-describe('Batches Test Suite', () => {
-  test('GET Batches', async () => {
+describe("Batches Test Suite", () => {
+  test("GET Batches", async () => {
     //test goes here
-    
-    let trainerEmail = 'mock1027.employee74df14df-5842-4811-a57c-be9836537a40@mock.com'
-    let returnValues;
-    let obj = {data: []};
-    axios.get = jest.fn().mockResolvedValue(obj);
-    await getBatches()
+
+    let trainerEmail =
+      "mock1027.employee74df14df-5842-4811-a57c-be9836537a40@mock.com";
+    //let returnValues;
+    //let obj = {data: []};
+    //axios.get = jest.fn().mockResolvedValue(obj);
+    //await getBatches(trainerEmail)
+
+    const batchIDs = ["TR-1111", "TR-1112", "TR-1113"];
+    // the response will be a list of a projection of batch objects
+    class BatchProjection {
+      constructor(
+        public batchId: string, //"TR-1131",
+        public name: string, //"Mock Batch 65",
+        public startDate: string, //"2021-04-09",
+        public endDate: string, //"2021-06-18",
+        public skill: string, // "Java React",
+        public location: string, // "New York",
+        public type: string // "Revature"
+      ) {
+        this.batchId = batchId;
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.skill = skill;
+        this.location = location;
+        this.type = type;
+      }
+    }
+    const batch1 = new BatchProjection(
+      "TR-1131",
+      "Mock Batch 100",
+      "2019-01-01",
+      "2019-04-01",
+      "React",
+      "New York",
+      "Revature"
+    );
+    const batch2 = new BatchProjection(
+      "TR-1112",
+      "Mock Batch 2",
+      "2021-06-07",
+      "2021-08-06",
+      "COBOL",
+      "Tampa",
+      "Revature"
+    );
+    const batch3 = new BatchProjection(
+      "TR-1111",
+      "Mock Batch 3",
+      "2021-05-01",
+      "2021-08-01",
+      "Java",
+      "West Virginia",
+      "Corporate"
+    );
+    const resp = { data: [batch1, batch2, batch3] };
+
+    axios.get.mockResolvedValue(resp);
+
+    // or you could use the following depending on your use case:
+    // axios.get.mockImplementation(() => Promise.resolve(resp))
+
+    return Users.all().then(data => expect(data).toEqual(users));
   });
 });
+
+/*
+import axios from 'axios';
+import Users from './users';
+
+jest.mock('axios');
+
+test('should fetch users', () => {
+  const users = [{name: 'Bob'}];
+  const resp = {data: users};
+  axios.get.mockResolvedValue(resp);
+
+  // or you could use the following depending on your use case:
+  // axios.get.mockImplementation(() => Promise.resolve(resp))
+
+  return Users.all().then(data => expect(data).toEqual(users));
+}); 
+*/
 
 // the next lines will go into the actual function, not the test
 /**
  * getBatches takes in a trainer ID (there is no trainer ID, only trainer email) and returns a list of batchIDs from the caliber mock api
  * @param event
  */
-//there will be an axios request to the caliber api endpoint batch/trainer/{traineremail}
- 
-
-
-// With the getRestaurants method there are two things we can test.
-// The first thing is that axios.get is actually called. If it isn't called the rest of the test isn't going to pass.
-// The second thing is that the data object is what is returned from the function.
-// We can also check the paramaters of the get function as a third thing (testing the environment variables are correct?)
-test('getRestaurants returns a promise with data in it.', async ()=>{
-    let returnValues;
-    
-    let obj = {data: []};
-    axios.get = jest.fn().mockResolvedValue(obj);
-    await restaurantService.getRestaurants().then((arr) => {
-        returnValues = arr;
-    });
-    expect(axios.get).toHaveBeenCalledTimes(1);
-    expect(returnValues).toBe(obj.data);
-    expect(axios.get).toHaveBeenCalledWith('https://k8n63gmyw8.execute-api.us-west-2.amazonaws.com/default/restaurants');
-})
+//pseudocode
+/**
+ * 1. axios.get(/batch/{trainerEmail}/ids)
+ * 2. Get response of batchID array
+ * 3. axios.get(/batch/{batchId}) for each id
+ *    -in .then of request take response and transform it into managable data object and
+ *  add to an array to be sent in handler of the Lambda function
+ */
