6d212b47ebba99640317c8c5852da247eb97f39c
Medbh Campbell <medbh.campbell53@gmail.com>
2021-02-12 09:57:03 -0600

    changed patchAssociate return from any to qcFeedback | null

--- associateLambda/index.ts
+++ associateLambda/index.ts
@@ -22,8 +22,8 @@ export const putAssociate = async (): Promise<any> => {};
 
 //method is patch
 //update an existing note or status
-export const patchAssociate = async (updateObject: string): Promise<any> => {
-
+export const patchAssociate = async (updateObject: string): Promise<qcFeedback | null> => {
+  return null;
 }
 
 export class qcFeedback {
