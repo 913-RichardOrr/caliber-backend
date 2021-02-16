668bf01150badb80aebe2260447d0968bb5d3cdb
Tngtater9 <jtater1@gmail.com>
2021-02-16 10:22:47 -0500

    renamed file and exported function

--- batches/getBatchesLambda.ts
+++ batches/getBatchesLambda.ts
@@ -4,17 +4,17 @@ function handler() {
 	// call getBatchesLambda and return response
 }
 
-function getBatchesLambda() {
+export function getBatchesLambda() {
 	let caliberURI: string =
 		'https://caliber2-mock.revaturelabs.com:443/mock/training/batch';
 
-	let batchIDs = [];
+	let batchIDs: string[] = [];
 	//get batchIDs
 
 	let batchInfo = [];
 
-	for (let batch of batchIDs) {
-		axios.get(`${caliberURI}/${batch}`).then(() => {
+	for (let batchID of batchIDs) {
+		axios.get(`${caliberURI}/${batchID}`).then(() => {
 			//transform batch info and add to batchInfo array
 		});
 	}
