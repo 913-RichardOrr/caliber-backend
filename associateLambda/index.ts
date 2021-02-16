050b754732bf3394d6a6b7aa1a9f8428fc12fbdf
Medbh Campbell <medbh.campbell53@gmail.com>
2021-02-16 09:43:09 -0600

    started working on function to parse path

--- associateLambda/index.ts
+++ associateLambda/index.ts
@@ -1,4 +1,5 @@
-import * as indexModule from './index'
+import * as indexModule from './index';
+import { Client } from 'pg';
 
 // lambda handler function
 
@@ -15,26 +16,47 @@ export const handler = async (event: AssocEvent): Promise<any> => {};
 
 //method is get
 //get the note and technical status for that person for that week
-export async function getAssociate(batchId: string, weekId: number, associateId: string): Promise<qcFeedback|null> {
+export async function getAssociate(batchId: string, weekId: number, associateId: string): Promise<QCFeedback|null> {
   return null;
-};
+}
 
 //method is put
 //create the note and technical status for that person for that week
-export async function putAssociate(): Promise<qcFeedback | null> {
-  let response = new qcFeedback();
+export async function putAssociate(): Promise<QCFeedback | null> {
+  let response = new QCFeedback();
   return response;
 }
 
 //method is patch
 //update an existing note or status
 export const patchAssociate = async (
+  path: string,
   updateObject: string
-): Promise<qcFeedback | null> => {
+): Promise<QCFeedback | null> => {
+  const client = new Client();
+
+
+
+  const q_note = 'update qcnotes set note = $1::text where associateid = $2::text and weekid = $3::integer and batchid = $3::text';
+  const q_status = 'update qcnotes set techstatus = $1::integer where associateid = $2::text and weekid = $3::integer and batchid = $3::text';
+
+  try {
+    await client.connect();
+    
+  }
+
   return null;
 }
 
-export class qcFeedback {
+function parsePath (path: string): Object {
+  const parts = path.split('/');
+  const associateId = parts[parts.length - 1];
+  const weekId = Number(parts[parts.length - 3]);
+  const batchId = Number(parts[parts.length - 5]);
+  return {batchId, weekId, associateId};
+}
+
+export class QCFeedback {
   batchId: string = '';
   weekId: number = 0;
   associateId: string = '';
