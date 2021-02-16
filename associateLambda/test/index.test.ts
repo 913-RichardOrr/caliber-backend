43a1924c58a91b9e8fe9c2a1e5c24aba40f706e2
kathrync23 <kathrynclark16@augustana.edu>
2021-02-16 14:01:03 -0600

    forgot to uncomment the other tests which I commented for easier readability when tests ran

--- associateLambda/test/index.test.ts
+++ associateLambda/test/index.test.ts
@@ -19,57 +19,57 @@ jest.mock('pg', () => {
 });
 
 //Author: Tyler
-// describe('tests for handler', () => {
-//   associateLambda.getAssociate = jest.fn().mockReturnValue('');
-//   associateLambda.putAssociate = jest.fn().mockReturnValue('');
-//   associateLambda.patchAssociate = jest.fn().mockReturnValue('');
+describe('tests for handler', () => {
+  associateLambda.getAssociate = jest.fn().mockReturnValue('');
+  associateLambda.putAssociate = jest.fn().mockReturnValue('');
+  associateLambda.patchAssociate = jest.fn().mockReturnValue('');
 
-//   test('test handler can differentiate between get/put/patch', async () => {
-//     testEvent = {
-//       path: '/something',
-//       body: '{1:1}',
-//       httpMethod: 'PUT',
-//     };
+  test('test handler can differentiate between get/put/patch', async () => {
+    testEvent = {
+      path: '/something',
+      body: '{1:1}',
+      httpMethod: 'PUT',
+    };
 
-//     await associateLambda.handler(testEvent);
+    await associateLambda.handler(testEvent);
 
-//     expect(associateLambda.getAssociate).toHaveBeenCalledTimes(0);
-//     expect(associateLambda.putAssociate).toHaveBeenCalledTimes(1);
-//     expect(associateLambda.patchAssociate).toHaveBeenCalledTimes(0);
-//   });
-//   test('test handler can differentiate between get/put/patch', async () => {
-//     testEvent = {
-//       path: '/something',
-//       body: '{1:1}',
-//       httpMethod: 'GET',
-//     };
+    expect(associateLambda.getAssociate).toHaveBeenCalledTimes(0);
+    expect(associateLambda.putAssociate).toHaveBeenCalledTimes(1);
+    expect(associateLambda.patchAssociate).toHaveBeenCalledTimes(0);
+  });
+  test('test handler can differentiate between get/put/patch', async () => {
+    testEvent = {
+      path: '/something',
+      body: '{1:1}',
+      httpMethod: 'GET',
+    };
 
-//     await associateLambda.handler(testEvent);
+    await associateLambda.handler(testEvent);
 
-//     expect(associateLambda.getAssociate).toHaveBeenCalledTimes(1);
-//     expect(associateLambda.putAssociate).toHaveBeenCalledTimes(0);
-//     expect(associateLambda.patchAssociate).toHaveBeenCalledTimes(0);
-//   });
-//   test('test handler can differentiate between get/put/patch', async () => {
-//     testEvent = {
-//       path: '/something',
-//       body: '{1:1}',
-//       httpMethod: 'PATCH',
-//     };
+    expect(associateLambda.getAssociate).toHaveBeenCalledTimes(1);
+    expect(associateLambda.putAssociate).toHaveBeenCalledTimes(0);
+    expect(associateLambda.patchAssociate).toHaveBeenCalledTimes(0);
+  });
+  test('test handler can differentiate between get/put/patch', async () => {
+    testEvent = {
+      path: '/something',
+      body: '{1:1}',
+      httpMethod: 'PATCH',
+    };
 
-//     await associateLambda.handler(testEvent);
+    await associateLambda.handler(testEvent);
 
-//     expect(associateLambda.getAssociate).toHaveBeenCalledTimes(0);
-//     expect(associateLambda.putAssociate).toHaveBeenCalledTimes(0);
-//     expect(associateLambda.patchAssociate).toHaveBeenCalledTimes(1);
-//   });
-// });
+    expect(associateLambda.getAssociate).toHaveBeenCalledTimes(0);
+    expect(associateLambda.putAssociate).toHaveBeenCalledTimes(0);
+    expect(associateLambda.patchAssociate).toHaveBeenCalledTimes(1);
+  });
+});
 
-// describe('tests for getAssociate', async () => {
-//   testEvent.path = '/something';
+describe('tests for getAssociate', async () => {
+  testEvent.path = '/something';
 
-//   test('that getAssociate gets associate');
-// });
+  test('that getAssociate gets associate');
+});
 
 describe('tests for putAssociate', () => {
   let path = '{"batchId":"batch1","weekId":1,"associateId":"testAssociateId"}';
@@ -103,77 +103,77 @@ describe('tests for putAssociate', () => {
   });
 });
 
-// describe('tests for patchAssociate', () => {
-//   const original: associateLambda.QCFeedback = {
-//     batchId: 'YYMM-mmmDD-Stuff',
-//     weekId: 1,
-//     associateId: 'example@example.net',
-//     qcNote: 'blablabla',
-//     qcTechnicalStatus: 2,
-//   };
+describe('tests for patchAssociate', () => {
+  const original: associateLambda.QCFeedback = {
+    batchId: 'YYMM-mmmDD-Stuff',
+    weekId: 1,
+    associateId: 'example@example.net',
+    qcNote: 'blablabla',
+    qcTechnicalStatus: 2,
+  };
 
-//   test("That updating an associate's note calls pg with correct query", async () => {
-//     const testUpdateObject = { qcNote: 'Updated blablabla' };
+  test("That updating an associate's note calls pg with correct query", async () => {
+    const testUpdateObject = { qcNote: 'Updated blablabla' };
 
-//     const updatedObject = original;
-//     updatedObject.qcNote = testUpdateObject.qcNote;
+    const updatedObject = original;
+    updatedObject.qcNote = testUpdateObject.qcNote;
 
-//     const res = await associateLambda.patchAssociate(
-//       JSON.stringify(testUpdateObject)
-//     );
-//     expect(res).toBe(updatedObject);
-//     expect(mockConnect).toHaveBeenCalledTimes(1);
-//     expect(mockQuery).toHaveBeenCalledTimes(1);
-//     expect(mockConnect).toHaveBeenCalledTimes(1);
-//     expect(
-//       mockQuery
-//     ).toHaveBeenLastCalledWith(
-//       'update qcnotes set note = $1::text where associateid = $2::text and weekid = $3::integer and batchid = $3::text',
-//       [
-//         testUpdateObject.qcNote,
-//         original.associateId,
-//         original.weekId,
-//         original.batchId,
-//       ]
-//     );
-//     expect(mockEnd).toHaveBeenCalledTimes(1);
-//   });
+    const res = await associateLambda.patchAssociate(
+      JSON.stringify(testUpdateObject)
+    );
+    expect(res).toBe(updatedObject);
+    expect(mockConnect).toHaveBeenCalledTimes(1);
+    expect(mockQuery).toHaveBeenCalledTimes(1);
+    expect(mockConnect).toHaveBeenCalledTimes(1);
+    expect(
+      mockQuery
+    ).toHaveBeenLastCalledWith(
+      'update qcnotes set note = $1::text where associateid = $2::text and weekid = $3::integer and batchid = $3::text',
+      [
+        testUpdateObject.qcNote,
+        original.associateId,
+        original.weekId,
+        original.batchId,
+      ]
+    );
+    expect(mockEnd).toHaveBeenCalledTimes(1);
+  });
 
-//   test("That updating an associate's status calls pg with correct query", async () => {
-//     const testUpdateObject = { qcTechnicalStatus: 3 };
+  test("That updating an associate's status calls pg with correct query", async () => {
+    const testUpdateObject = { qcTechnicalStatus: 3 };
 
-//     const updatedObject = original;
-//     updatedObject.qcTechnicalStatus = testUpdateObject.qcTechnicalStatus;
+    const updatedObject = original;
+    updatedObject.qcTechnicalStatus = testUpdateObject.qcTechnicalStatus;
 
-//     const res = await associateLambda.patchAssociate(
-//       JSON.stringify(testUpdateObject)
-//     );
-//     expect(res).toBe(updatedObject);
-//     expect(mockConnect).toHaveBeenCalledTimes(1);
-//     expect(mockQuery).toHaveBeenCalledTimes(1);
-//     expect(
-//       mockQuery
-//     ).toHaveBeenLastCalledWith(
-//       'update qcnotes set techstatus = $1::integer where associateid = $2::text and weekid = $3::integer and batchid = $3::text',
-//       [
-//         testUpdateObject.qcTechnicalStatus,
-//         original.associateId,
-//         original.weekId,
-//         original.batchId,
-//       ]
-//     );
-//     expect(mockEnd).toHaveBeenCalledTimes(1);
-//   });
+    const res = await associateLambda.patchAssociate(
+      JSON.stringify(testUpdateObject)
+    );
+    expect(res).toBe(updatedObject);
+    expect(mockConnect).toHaveBeenCalledTimes(1);
+    expect(mockQuery).toHaveBeenCalledTimes(1);
+    expect(
+      mockQuery
+    ).toHaveBeenLastCalledWith(
+      'update qcnotes set techstatus = $1::integer where associateid = $2::text and weekid = $3::integer and batchid = $3::text',
+      [
+        testUpdateObject.qcTechnicalStatus,
+        original.associateId,
+        original.weekId,
+        original.batchId,
+      ]
+    );
+    expect(mockEnd).toHaveBeenCalledTimes(1);
+  });
 
-//   test("That invalid input returns null but doesn't break anything", async () => {
-//     const testUpdateObject = { nonsense: 3 };
+  test("That invalid input returns null but doesn't break anything", async () => {
+    const testUpdateObject = { nonsense: 3 };
 
-//     const res = await associateLambda.patchAssociate(
-//       JSON.stringify(testUpdateObject)
-//     );
-//     expect(res).toBe(null);
-//     expect(mockConnect).toHaveBeenCalledTimes(0);
-//     expect(mockQuery).toHaveBeenCalledTimes(0);
-//     expect(mockEnd).toHaveBeenCalledTimes(0);
-//   });
-// });
+    const res = await associateLambda.patchAssociate(
+      JSON.stringify(testUpdateObject)
+    );
+    expect(res).toBe(null);
+    expect(mockConnect).toHaveBeenCalledTimes(0);
+    expect(mockQuery).toHaveBeenCalledTimes(0);
+    expect(mockEnd).toHaveBeenCalledTimes(0);
+  });
+});
