94b015d5c31fe4176746e7ff41a653508be76858
KirstenWood <kirstenhope98@gmail.com>
2021-02-18 16:43:59 -0500

    added more comments

--- response.ts
+++ response.ts
@@ -1,3 +1,10 @@
+/**
+ * When the function is called an http response is created
+ * @param body- message to be in the body of the http response
+ * @param statusCode- status code to be returned
+ * @param header- any extra header to be added to our baseHeader
+ */
+
 export default function createResponse(body: any = {}, statusCode: number = 200, headers: any = {}) {
     const baseHeader: any = {
         'Content-Type': 'application/json',
