da9b1e0b57b3c88f7f5c1edc580c4833f4963698
KirstenWood <kirstenhope98@gmail.com>
2021-02-24 14:25:43 -0500

    now there is only one lambda file\

---
+++ WeekCategories/WeekCategoriesHelper.ts
@@ -0,0 +1,54 @@
+import createResponse from "../response";
+
+
+interface categoryParams{
+    weekID: number,
+    categoryID: number
+}
+
+interface getCategoryParams{
+    weekID: number
+
+}
+
+export const addCategory = async (client: any, params: categoryParams) => 
+{
+   
+    const query = `insert into week_categories (category_id, qc_week_id) values ($1, $2)`;
+    const values = [ params.categoryID, params.weekID ] ;
+
+    let response = await client.query(query, values);
+    client.end();
+    if (response) {
+        return createResponse(JSON.stringify(response.rows), 200);
+    } else {
+        return createResponse('', 400);
+    }  
+}
+
+export const getCategories = async (client: any, params: getCategoryParams) => {
+
+    let response;
+
+    //we want the name of category and the id
+    response = await client.query(`select skill, id from category where id = (select category_id from week_categories where week_id = '${params.weekID}')`);
+    client.end();
+
+    if (response){
+        return createResponse(JSON.stringify(response.rows), 200);
+    } else{
+        return createResponse('', 400);
+    };
+}
+
+export const deleteCategory = async (client: any, params: categoryParams) => 
+{
+    await client.query(`delete from week_categories where category_id = ${params.categoryID} and qc_week_id =  ${params.weekID}`).then((response: any) => {
+        client.end();
+        if(response){
+          return createResponse('', 200);      
+        }else{
+          return createResponse('', 400)
+        }
+      });
+}
---
+++ WeekCategories/WeekCategoriesLambda.ts
@@ -0,0 +1,29 @@
+import {getCategories, addCategory, deleteCategory} from './WeekCategoriesHelper';
+
+export const handler = async (event:any)=>{
+    const { Client } = require('pg');
+    const client = new Client();
+    await client.connect();
+
+    const method = event.httpMethod;
+    switch (method) {
+        case 'GET':
+            let id = event.path.substring(event.path.lastIndexOf('/') + 1, event.path.length);
+            getCategories(client,id);
+            break;
+        case 'POST':
+            let postCategory = JSON.parse(event.body);
+            let postWeek = event.path.substring(event.path.lastIndexOf('/')+ 1, event.path.length);
+            addCategory(client,{weekID: postWeek, categoryID: postCategory});
+            break;
+        case 'DELETE':
+            let deleteVarCategory = JSON.parse(event.body);
+            let deleteWeek = event.path.substring(event.path.lastIndexOf('/') + 1, event.path.length);
+            deleteCategory(client, {weekID: deleteWeek, categoryID: deleteVarCategory})
+            break;
+        default:
+            console.log(`Does not support HTTP method ${method}`);
+            client.end();
+            break;
+    }
+}
