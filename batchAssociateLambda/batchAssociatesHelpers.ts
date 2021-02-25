88ee88202380a1ad4a82f5f94849a2a1841e18e6
relytnroh <relytnroh@gmail.com>
2021-02-25 14:45:21 -0800

    added batchassociateLambda

--- batchAssociateLambda/batchAssociatesHelpers.ts
+++ batchAssociateLambda/batchAssociatesHelpers.ts
@@ -11,16 +11,8 @@ export interface AssociateEvent {
  * get the note and technical status for that person for that week
  * @param path is the string path with the batch/week/associate information.
  */
-export async function getAssociates(path: string): Promise<QCFeedback | null> {
+export async function getAssociates(path: string): Promise<any[] | null> {
   let URI: string = 'https://aosczl5fvf.execute-api.us-west-2.amazonaws.com/default';
   let newpath = path.split('/');
   return axios.get(URI +"/batch/"+newpath[newpath.length-1], { withCredentials: true }).then((result: { data: any; }) => result.data).catch((err: any) => { console.error(err) });
-}
-
-export interface QCFeedback {
-  batchid: string;
-  weeknumber: number;
-  associateid: string;
-  notecontent: string;
-  technicalstatus: number;
 }
