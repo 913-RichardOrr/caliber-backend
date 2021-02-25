61cd75326bc8dc87f891c616accfcf444da6d57a
relytnroh <relytnroh@gmail.com>
2021-02-25 14:40:27 -0800

    batchAssociateLambda

---
+++ batchAssociateLambda/batchAssociateLambda.ts
@@ -0,0 +1,21 @@
+import createResponse from './response';
+import { getAssociates } from './batchAssociateHelpers'
+
+export interface AssociateEvent {
+  path: string;
+  httpMethod: string;
+  body?: string;
+}
+
+/**
+ * figures out what http method has been called: GET, PUT, PATCH, then 
+ * calls the relevant helper function return the relevant object
+ * @param event 
+ */
+export async function handler(event: AssociateEvent): Promise<any> {
+  const associates = await getAssociates(event.path);
+  if (associates) {
+    return createResponse(JSON.stringify(associates), 200);
+  }
+  return createResponse('', 404);
+};
