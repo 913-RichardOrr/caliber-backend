bc05a211893f40b1e6ad657baeab29c6e60be306
Emma (Yuqing) Wang <wangyuqingxiao@gmail.com>
2021-03-01 00:54:20 -0500

    lambda handler test

--- authorizer/test/authorizerHandler.test.ts
+++ authorizer/test/authorizerHandler.test.ts
@@ -1,57 +1,88 @@
 const serviceAccount = require('firebase-service-account-DO_NOT_PUSH.json');
-import {handler} from '../authorizerHandler';
+import { handler } from '../authorizerHandler';
 var module = require('../authorizerHelper');
 import admin from 'firebase-admin';
 
-jest.mock('firebase-service-account-DO_NOT_PUSH.json', ()=>({
-    settings: 'someSetting'
-}), { virtual: true })
+const vp = {
+    ROLE_QC: false,
+    ROLE_VP: true,
+    ROLE_TRAINER: false
+};
 
-// jest.mock('firebase-admin', () => ({
-//     admin: {
-//         initializeApp: jest.fn(),
-//         auth: {
-//             verifyIdToken: jest
-//                 .fn()
-//                 .mockImplementation(() =>
-//                     Promise.resolve({ qc: true, vp: false, trainer: false })
-//                 )
-//         }        
-//     }
-// }));
+jest.mock('firebase-admin', () => ({
+    apps: [],
+    credential: {
+        cert: jest.fn()
+    },
+    initializeApp: jest.fn(),
+    auth: jest.fn(() => ({
+        verifyIdToken: jest
+            .fn()
+            .mockImplementation((token: string) => Promise.resolve(vp))
+    }))
+}));
 
-jest.mock("firebase-admin");
+jest.mock(
+    'firebase-service-account-DO_NOT_PUSH.json',
+    () => ({
+        settings: 'someSetting'
+    }),
+    { virtual: true }
+);
 
+const event = {
+    authorizationToken: 'Bearer tokenvalue',
+    methodArn: 'anything'
+};
 
-jest.mock('../authorizerHelper', ()=>{
-    return jest.fn().mockReturnValue('Allow');
-});
+const noBearer = {
+    authorizationToken: 'tokenvalue',
+    methodArn: 'anything'
+};
+const noToken = {
+    methodArn: 'anything'
+};
+const context = {
+    fail: jest.fn()
+};
 
 describe('Authorizer Handler Test Suite', () => {
-    const vp = {
-        qc: false,
-        vp: true,
-        trainer: false
-    };
-    beforeEach(() => {
-        admin.initializeApp = jest.fn();
-        admin.auth = jest.fn();
-        admin.auth().verifyIdToken = jest.fn().mockReturnValue(Promise.resolve(vp));
-    })
-
-    test('authorizer for a ', async () => {
+    beforeAll(() => {
         module.helper = jest.fn();
-        module.generateIamPolicy = jest.fn();
-        const event = {
-            authorizationToken: 'Bearer tokenvalue',
-            methodArn: 'anything'
-        }
-        const context = {
-            fail: jest.fn()
-        }
+        module.generateIamPolicy = jest
+            .fn()
+            .mockImplementation((effect, resource) => {
+                return 'anything';
+            });
+    });
+
+    test('when the token is in the wrong format', async () => {
+        await handler(noBearer, context);
+        expect(context.fail).toBeCalled();
+        expect(context.fail).toBeCalledWith('Unauthorized');
+        await handler(noToken, context);
+        expect(context.fail).toBeCalled();
+        expect(context.fail).toBeCalledWith('Unauthorized');
+    });
+
+    test('establish connection when admin.apps.length is 0', async () => {
+        await handler(event, context);
+        expect(admin.initializeApp).toBeCalled();
+        expect(admin.apps.length).toBe(0);
+        expect(admin.auth().verifyIdToken).toBeCalled();
+        expect(admin.auth().verifyIdToken).toBeCalledWith(
+            event.authorizationToken.split(' ')[1]
+        );
+    });
+
+    test('user with vp role is allow access to all route', async () => {
         await handler(event, context);
         expect(module.generateIamPolicy).toBeCalled();
-        expect(admin.auth().verifyIdToken).toBeCalled();
-        expect(module.helper).toBeCalled();
+        expect(module.generateIamPolicy).toBeCalledWith(
+            'Allow',
+            event.methodArn
+        );
+        //will not call helper function, vp returns allow before that
+        expect(module.helper.mock.calls.length).toBe(0);
     });
 });
