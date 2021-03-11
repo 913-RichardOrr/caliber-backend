09bafd0518ff9b9a3b24896d297c4e40b8f9603b
Jonathan Dawson <dawsonjed@gmail.com>
2021-03-10 23:34:06 -0600

    start script

--- backendSetup.sh
+++ backendSetup.sh
@@ -1,1 +1,23 @@
-#setting up resources on cloudformation
+#!/usr/bin/env bash
+
+stackName = "caliberMobile"
+roleArn = "arn:aws:iam::855430746673:role/caliber-mobile-cf"
+
+#import the environment variables from the file
+source env.example
+echo $PASSWORD
+# setting up database  on cloudformation
+# aws cloudformation deploy --template-file ./cloudFormation/rds.yaml --stack-name $stackName \
+#     --role-arn $roleArn --parameter-overrides DBUsername=$USER DBPassword=$PASSWORD
+
+#associative array of ECR names as keys and dockerfile paths as values
+declare -A dockerfileArray
+dockerfileArray[AssociatesECR] = "./associateLambda/Dockerfile"
+dockerfileArray[FireBaseECR] = "./authorizer/Dockerfile"
+dockerfileArray[BatchAssociatesECR] = "./batchAssociatesLambda/Dockerfile"
+dockerfileArray[BatchesECR] = "./batches/docker/Dockerfile"
+dockerfileArray[BatchWeekECR] = "./batchWeek/lambda/Dockerfile"
+dockerfileArray[CategoriesECR] = "./categoriesFeature/Dockerfile"
+dockerfileArray[WeekCategoriesECR] = "./weekCategories/docker/Dockerfile"
+echo $dockerfileArray
+#loop to build docker images
